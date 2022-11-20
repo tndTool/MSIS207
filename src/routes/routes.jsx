29ea2936e4a070854813787363998eb4a4cbabd4
taturo2001 <92012553+taturo2001@users.noreply.github.com>
2022-11-20 22:57:24 +0700

    reactjs updated: routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,28 +2,18 @@ import React from 'react';
 
 import { Route, Switch } from 'react-router-dom';
 
-import Home from '~/pages/Home';
-import Top from '~/pages/Top';
-import Outwear from '~/pages/Outwear';
-import Bottom from '~/pages/Bottom';
-import Accessories from '~/pages/Accessories';
-import Cart from '~/pages/Cart';
-import Product from '~/pages/Product';
+import Home from '../pages/Home';
+import Top from '../pages/Top';
+import Cart from '../pages/Cart';
+import Product from '../pages/Product';
 
 const Routes = () => {
     return (
         <Switch>
             <Route path="/" exact component={Home} />
-            <Route path="/top" exact component={Top} />
-            <Route path="/outwear" exact component={Outwear} />
-            <Route path="/bottom" exact component={Bottom} />
-            <Route path="/accessories" exact component={Accessories} />
-            <Route path="/cart" exact component={Cart} />
-
-            <Route path="/top/:slug" exact component={Product} />
-            <Route path="/outwear/:slug" exact component={Product} />
-            <Route path="/bottom/:slug" exact component={Product} />
-            <Route path="/cart/:slug" exact component={Product} />
+            <Route path="/top/:slug" component={Product} />
+            <Route path="/top" component={Top} />
+            <Route path="/cart" component={Cart} />
         </Switch>
     );
 };
