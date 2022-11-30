c1a5330a338d36c39371b6d9372b741cd4c36210
Shu <thanhtrung230100@gmail.com>
2022-11-30 16:31:34 +0700

    update

--- src/pages/Authen/Login/index.js
+++ src/pages/Authen/Login/index.js
@@ -6,43 +6,39 @@ const cx = classNames.bind(styles);
 
 export default function Login() {
     return (
-        <div className={cx('')}>
-            <section>
-                <div className={cx('container')}>
-                    <div className={cx('box')}>
-                        <div className={cx('form-login')}>
-                            <h2>SIGN IN</h2>
+        <div className={cx('container')}>
+            <div className={cx('box')}>
+                <div className={cx('form-login')}>
+                    <h2>SIGN IN</h2>
 
-                            <div className={cx('form-login__inputBox')}>
-                                <input type="text" required="required" />
-                                <span>Email </span>
-                                <i></i>
-                            </div>
+                    <div className={cx('form-login__inputBox')}>
+                        <input type="text" required="required" />
+                        <span>Email </span>
+                        <i></i>
+                    </div>
 
-                            <div className={cx('form-login__inputBox')}>
-                                <input type="password" required="required" />
-                                <span>Password</span>
-                                <i></i>
-                            </div>
+                    <div className={cx('form-login__inputBox')}>
+                        <input type="password" required="required" />
+                        <span>Password</span>
+                        <i></i>
+                    </div>
 
-                            <div className={cx('form-login__links')}>
-                                <Link to="#">
-                                    <span>Forgot password?</span>
-                                </Link>
-                                <Link to="/register">
-                                    <span>SIGN UP</span>
-                                </Link>
-                            </div>
+                    <div className={cx('form-login__links')}>
+                        <Link to="#">
+                            <span>Forgot password?</span>
+                        </Link>
+                        <Link to="/register">
+                            <span>SIGN UP</span>
+                        </Link>
+                    </div>
 
-                            <div className={cx('form-login__btn')}>
-                                <Link to="/">
-                                    <span>LOGIN</span>
-                                </Link>
-                            </div>
-                        </div>
+                    <div className={cx('form-login__btn')}>
+                        <Link to="/">
+                            <span>LOGIN</span>
+                        </Link>
                     </div>
                 </div>
-            </section>
+            </div>
         </div>
     );
 }
--- src/pages/Authen/Register/index.js
+++ src/pages/Authen/Register/index.js
@@ -7,46 +7,52 @@ const cx = classNames.bind(styles);
 
 export default function Register() {
     return (
-        <div className={cx('')}>
-            <section>
-                <div className={cx('container')}>
-                    <div className={cx('box')}>
-                        <div className={cx('form-register')}>
-                            <h2>REGISTER</h2>
+        <div className={cx('container')}>
+            <div className={cx('box1')}>
+                <div className={cx('form-register')}>
+                    <h2>REGISTER</h2>
 
-                            <div className={cx('form-register__inputBox')}>
-                                <input type="text" required="required" />
-                                <span>Username </span>
-                                <i></i>
-                            </div>
+                    <div className={cx('form-register__inputBox')}>
+                        <input type="text" required="required" />
+                        <span>Username </span>
+                        <i></i>
+                    </div>
 
-                            <div className={cx('form-register__inputBox')}>
-                                <input type="text" required="required" />
-                                <span>Email </span>
-                                <i></i>
-                            </div>
+                    <div className={cx('form-register__inputBox')}>
+                        <input type="text" required="required" />
+                        <span>Email </span>
+                        <i></i>
+                    </div>
 
-                            <div className={cx('form-register__inputBox')}>
-                                <input type="password" required="required" />
-                                <span>Password</span>
-                                <i></i>
-                            </div>
+                    <div className={cx('form-register__inputBox')}>
+                        <input type="password" required="required" />
+                        <span>Password</span>
+                        <i></i>
+                    </div>
 
-                            <div className={cx('form-register__inputBox')}>
-                                <input type="password" required="required" />
-                                <span>Re-password</span>
-                                <i></i>
-                            </div>
+                    <div className={cx('form-register__inputBox')}>
+                        <input type="password" required="required" />
+                        <span>Re-password</span>
+                        <i></i>
+                    </div>
 
-                            <div className={cx('form-register__btn')}>
-                                <Link to="/">
-                                    <span>SIGN UP</span>
-                                </Link>
-                            </div>
-                        </div>
+                    <div className={cx('form-register__links')}>
+                        <Link to="#">
+                            <span>Already account?</span>
+                        </Link>
+                        <Link to="/login">
+                            <span>LOGIN</span>
+                        </Link>
+                    </div>
+
+                    <div className={cx('form-register__btn')}>
+                        <Link to="/">
+                            <span>SIGN UP</span>
+                        </Link>
                     </div>
                 </div>
-            </section>
+            </div>
+            <div className={cx('box2')}></div>
         </div>
     );
 }
