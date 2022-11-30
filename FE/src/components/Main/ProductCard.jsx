fea706ad7ee52304694cffc0789c801f9622d3d2
Trần Lương Tiến Sĩ - 19522128 <91487292+tranluongtiensi@users.noreply.github.com>
2022-11-30 19:54:53 +0700

    ljhg

--- FE/src/components/Main/ProductCard.jsx
+++ FE/src/components/Main/ProductCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom';
 
-import Button from './button';
+import Button from './Button';
 
 import numberWithCommas from '~/utils/numberWithCommas';
 
