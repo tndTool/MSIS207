baefe667a425fd32b2e435f123f1785d952534a3
taturo2001 <92012553+I0ntdnt0I@users.noreply.github.com>
2022-11-30 19:40:07 +0700

    add register form

---
+++ src/pages/Authen/Register/index.js
@@ -0,0 +1,57 @@
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import styles from './Register.module.scss';
+
+const cx = classNames.bind(styles);
+
+export default function Register() {
+    return (
+        <div className={cx('container')}>
+            <div className={cx('box1')}>
+                <div className={cx('form-register')}>
+                    <h2>REGISTER</h2>
+
+                    <div className={cx('form-register__inputBox')}>
+                        <input type="text" required="required" />
+                        <span>Username </span>
+                        <i></i>
+                    </div>
+
+                    <div className={cx('form-register__inputBox')}>
+                        <input type="text" required="required" />
+                        <span>Email </span>
+                        <i></i>
+                    </div>
+
+                    <div className={cx('form-register__inputBox')}>
+                        <input type="password" required="required" />
+                        <span>Password</span>
+                        <i></i>
+                    </div>
+
+                    <div className={cx('form-register__inputBox')}>
+                        <input type="password" required="required" />
+                        <span>Re-password</span>
+                        <i></i>
+                    </div>
+
+                    <div className={cx('form-register__links')}>
+                        <Link to="#">
+                            <span>Already account?</span>
+                        </Link>
+                        <Link to="/login">
+                            <span>LOGIN</span>
+                        </Link>
+                    </div>
+
+                    <div className={cx('form-register__btn')}>
+                        <Link to="/">
+                            <span>SIGN UP</span>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className={cx('box2')}></div>
+        </div>
+    );
+}
