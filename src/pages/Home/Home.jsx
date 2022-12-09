79d8da79808d3120e973c1e5f44b79b8574d194f
taturo2001 <92012553+I0ntdnt0I@users.noreply.github.com>
2022-12-09 13:07:13 +0700

    Reactjs project 09/12/2022: Code optimization!

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,11 +8,56 @@ import Policy from '~/components/Main/Policy';
 import ProductCard from '~/components/Main/ProductCard';
 import Grid from '~/components/Main/Grid';
 
-import homeSliderData from '~/assets/fake-data/home-slider';
-import policy from '~/assets/fake-data/policy';
 import productData from '~/assets/fake-data/products';
 
 const Home = () => {
+    const policy = [
+        {
+            name: 'Miễn phí giao hàng',
+            description: 'Miễn phí ship với đơn hàng > 199K',
+            icon: 'bx bx-shopping-bag',
+        },
+        {
+            name: 'Thanh toán COD',
+            description: 'Thanh toán khi nhận hàng (COD)',
+            icon: 'bx bx-credit-card',
+        },
+        {
+            name: 'Khách hàng VIP',
+            description: 'Ưu đãi dành cho khách hàng VIP',
+            icon: 'bx bx-diamond',
+        },
+        {
+            name: 'Hỗ trợ bảo hành',
+            description: 'Đổi, sửa đồ tại tất cả store',
+            icon: 'bx bx-donate-heart',
+        },
+    ];
+
+    const homeSliderData = [
+        {
+            title: 'Áo Xanh',
+            description: 'Thời trang phang thời tiết',
+            img: 'https://res.cloudinary.com/dcwka06ph/image/upload/v1669648383/Website_project/Slider/heroslider_1_ulyyoj.png',
+            color: 'darkcyan',
+            path: '/top',
+        },
+        {
+            title: 'Áo Hồng',
+            description: 'Thời trang phang thời tiết',
+            img: 'https://res.cloudinary.com/dcwka06ph/image/upload/v1669343239/Website_project/Slider/slide_2_shsclv.png',
+            color: 'pink',
+            path: '/top',
+        },
+        {
+            title: 'Áo Cam',
+            description: 'Thời trang phang thời tiết',
+            img: 'https://res.cloudinary.com/dcwka06ph/image/upload/v1669648373/Website_project/Slider/heroslider_2_yzxzeb.png',
+            color: 'orange',
+            path: '/top',
+        },
+    ];
+
     return (
         <Helmet title="Home">
             {/* Home slider */}
@@ -37,7 +82,7 @@ const Home = () => {
             <Section>
                 <SectionTitle>top sản phẩm bán chạy trong tuần</SectionTitle>
                 <SectionBody>
-                    <Grid col={4} mdCol={2} smCol={1} gap={20}>
+                    <Grid col={4} mdCol={2} smCol={2} gap={20}>
                         {productData.getProducts(4).map((item, index) => (
                             <ProductCard
                                 key={index}
@@ -57,7 +102,7 @@ const Home = () => {
             <Section>
                 <SectionTitle>sản phẩm mới</SectionTitle>
                 <SectionBody>
-                    <Grid col={4} mdCol={2} smCol={1} gap={20}>
+                    <Grid col={4} mdCol={2} smCol={2} gap={20}>
                         {productData.getProducts(4).map((item, index) => (
                             <ProductCard
                                 key={index}
@@ -77,7 +122,7 @@ const Home = () => {
             <Section>
                 <SectionTitle>phổ biến</SectionTitle>
                 <SectionBody>
-                    <Grid col={4} mdCol={2} smCol={1} gap={20}>
+                    <Grid col={4} mdCol={2} smCol={2} gap={20}>
                         {productData.getProducts(4).map((item, index) => (
                             <ProductCard
                                 key={index}
