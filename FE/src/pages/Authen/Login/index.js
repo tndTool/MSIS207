6b2224312db4196d32eb18ec8c02c33d3d7e533a
Trần Lương Tiến Sĩ - 19522128 <91487292+tranluongtiensi@users.noreply.github.com>
2022-12-13 11:53:22 +0700

    update login and register BE

--- FE/src/pages/Authen/Login/index.js
+++ FE/src/pages/Authen/Login/index.js
@@ -1,29 +1,100 @@
 import classNames from 'classnames/bind';
-import { Link } from 'react-router-dom';
+import { useState} from 'react';
+import { Link,useHistory } from 'react-router-dom';
 import styles from './Login.module.scss';
+import request from '~/utils/request';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 
-const cx = classNames.bind(styles);
+
 
 export default function Login() {
+    
+    const cx = classNames.bind(styles);
+
+    const [inputs, setInputs] = useState({
+        email:"",
+        password:"",
+      })
+
+      const [formErrors, setFormErrors] = useState({});
+      const [isSubmit, setIsSubmit] = useState(false);
+      const [submitError, setSubmitError] = useState();
+      const [resStatus, setResStatus] = useState();
+      
+      console.log(formErrors)
+      const history = useHistory();
+
+      const handleChange = e => {
+        setInputs(prev=>({...prev, [e.target.name]: e.target.value}));
+        if(e.target.value !== 0 && Object.keys(formErrors).length !== 0){
+        setFormErrors(prev=>({...prev, [e.target.name]: null}))
+        }
+      };
+      const handleSubmit = async e => {
+          
+        e.preventDefault();
+        try {
+          const res = await request.post("/auth/login", inputs);
+          setTimeout(() => history.push("/"), 800);
+
+          setResStatus(res.status);
+        } catch (err) {
+          setSubmitError(err.response.data);
+        }
+        setFormErrors(validate(inputs));
+        setIsSubmit(true);
+      };
+      
+  
+      const validate = (values) => {
+          const errors = {};
+          //thông báo lỗi nếu thông tin đăng kí sai hay k nhập
+          if (!values.email) {
+              errors.email = "Enter your email";
+          } 
+          if (!values.password){
+              errors.password = "Enter your password";
+          }
+
+          return errors;
+      }
     return (
         <div className={cx('container')}>
             <div className={cx('box')}>
                 <div className={cx('form-login')}>
                     <h2>SIGN IN</h2>
 
+                    
+            { resStatus !== 200 && isSubmit ? 
+            (<div className={cx('ui-message_error')}>
+                <div> <HighlightOffIcon className={cx('ui-message_error-icon')}/></div>
+                <div>{submitError}</div>
+            </div>): 
+            (null)}
+
+            {resStatus === 200 && isSubmit ? 
+            (<div className={cx('ui-message_success')}>
+                <div> <CheckCircleOutlineIcon className={cx('ui-message_success-icon')}/></div>
+                <div>Signed in succes</div>
+            </div>): 
+            (null)}
+
                     <div className={cx('form-login__inputBox')}>
-                        <input type="text" required="required" />
+                        <input type="text" required="required" name='email' onChange={handleChange}/>
 
                         <span>Email </span>
 
                         <i></i>
                     </div>
+                    <p>{formErrors.email}</p>
 
                     <div className={cx('form-login__inputBox')}>
-                        <input type="password" required="required" />
+                        <input type="password" required="required" name='password' onChange={handleChange}/>
                         <span>Password</span>
                         <i></i>
                     </div>
+                    <p>{formErrors.password}</p>
 
                     <div className={cx('form-login__links')}>
                         <Link to="#">
@@ -35,7 +106,7 @@ export default function Login() {
                     </div>
 
                     <div className={cx('form-login__btn')}>
-                        <span>LOGIN</span>
+                        <span onClick={handleSubmit} >LOGIN</span>
                     </div>
                 </div>
             </div>
--- FE/src/pages/Authen/Register/index.js
+++ FE/src/pages/Authen/Register/index.js
@@ -1,39 +1,128 @@
 import classNames from 'classnames/bind';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import styles from './Register.module.scss';
+import request from '~/utils/request';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 
-const cx = classNames.bind(styles);
 
-export default function Register() {
+function Register() {
+    const cx = classNames.bind(styles);
+
+    const [inputs, setInputs] = useState({
+      username:"",
+      email:"",
+      password:"",
+      re_password:""
+    })
+
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+    const [submitError, setSubmitError] = useState();
+    const [status, setStatus] = useState();
+    
+    const history = useHistory();
+
+
+    const handleChange = e => {
+      setInputs(prev=>({...prev, [e.target.name]: e.target.value}));
+      // remove error below input if key change
+      if(e.target.value !== 0 && Object.keys(formErrors).length !== 0){
+      setFormErrors(prev=>({...prev, [e.target.name]: null}))
+      }
+    };
+    const handleSubmit = async e => {
+        
+      e.preventDefault();
+      // if register succes run 'try' without run 'catch'
+      try {
+        const res = await request.post("/auth/register", inputs);
+        setStatus(res.status);
+        setTimeout(() => history.push("/login"), 1000);
+
+      } catch (err) {
+        setSubmitError(err.response.data);
+      }
+      setFormErrors(validate(inputs));
+      setIsSubmit(true);
+
+    };
+
+    const validate = (values) => {
+        const errors = {};
+        const regexEmail = /^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$/;
+        const regexUsername = /^[a-zA-Z0-9]{3,20}$/;
+        // error below input
+        if (!values.username) {
+            errors.username = "Enter your username"
+        } else if (!regexUsername.test(values.username)){
+            errors.username = "Username should be 3-20 characters and shouldn't include any specical character";
+        }
+        if (!values.email) {
+            errors.email = "Enter your email";
+        } else if (!regexEmail.test(values.email)){
+            errors.email = "Invalid email address. Please correct and try again.";
+        }
+        if (!values.password){
+            errors.password = "Enter your password";
+        } else if (values.password.length < 6) {
+            errors.password = "Minimum 6 characters required";
+        }
+        if (!values.re_password && values.password){
+            errors.re_password = "Type your password again";
+        }
+
+        return errors;
+    }
+
+
     return (
         <div className={cx('container')}>
             <div className={cx('box')}>
                 <div className={cx('form-register')}>
                     <h2>REGISTER</h2>
 
+            {status !== 200 && isSubmit ? 
+            (<div className={cx('ui-message_error')}>
+                <div> <HighlightOffIcon className={cx('ui-message_error-icon')}/></div>
+                <div>{submitError}</div>
+            </div>): 
+            (null)}
+
+            { status === 200 && isSubmit ? 
+            (<div className={cx('ui-message_success')}>
+                <div> <CheckCircleOutlineIcon className={cx('ui-message_success-icon')}/></div>
+                <div>User created successfully, please login</div>
+            </div>): 
+            (null)}
                     <div className={cx('form-register__inputBox')}>
-                        <input type="text" required="required" />
+                        <input type="text" required="required" name="username" onChange={handleChange} />
                         <span>Username </span>
                         <i></i>
                     </div>
+                    <p>{formErrors.username}</p>
 
                     <div className={cx('form-register__inputBox')}>
-                        <input type="text" required="required" />
+                        <input type="text" required="required" name="email" onChange={handleChange}/>
                         <span>Email </span>
                         <i></i>
                     </div>
+                    <p>{formErrors.email}</p>
 
                     <div className={cx('form-register__inputBox')}>
-                        <input type="password" required="required" />
+                        <input type="password" required="required" name="password" onChange={handleChange}/>
                         <span>Password</span>
                         <i></i>
                     </div>
+                    <p>{formErrors.password}</p>
 
                     <div className={cx('form-register__inputBox')}>
-                        <input type="password" required="required" />
+                        <input type="password" required="required" name="re_password" onChange={handleChange}/>
                         <span>Re-password</span>
                         <i></i>
                     </div>
+                    <p>{formErrors.re_password}</p>
 
                     <div className={cx('form-register__links')}>
                         <Link to="/login">
@@ -42,12 +131,11 @@ export default function Register() {
                     </div>
 
                     <div className={cx('form-register__btn')}>
-                        <Link to="#">
-                            <span>SIGN UP</span>
-                        </Link>
+                            <span onClick={handleSubmit}>SIGN UP</span>
                     </div>
                 </div>
             </div>
         </div>
     );
 }
+export default Register;
