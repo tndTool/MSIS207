15ea192cfa89c30569b059a61d39b24677377a87
Trần Lương Tiến Sĩ - 19522128 <91487292+tranluongtiensi@users.noreply.github.com>
2022-12-27 07:32:09 +0700

    jk

--- FE/src/utils/request.js
+++ FE/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const request = axios.create({
-    baseURL: 'http://localhost:8800/api',
+    baseURL: process.env.REACT_APP_API_URL,
     withCredentials: true, 
     credentials: 'include'
 })
